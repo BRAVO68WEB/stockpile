5a823ecde54972460bdeead8ada4f270b8b0c3ec
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-03-11 13:53:55 +0530

    ...

--- app/main.js
+++ app/main.js
@@ -2,13 +2,24 @@ const net = require("net");
 
 console.log("Logs from your program will appear here!");
 
+const parseIncomingData = (data) => {
+    const lines = data.toString().split("\r\n");
+    // DEBUG input data
+    console.log(lines);
+    return lines;
+}
+
+let id = 0;
+
 const server = net.createServer((connection) => {
-  connection.on("data", (data) => {
-    // console.log(JSON.stringify(data.toString()));
-    const command = data.toString().split("\r\n")[2];
-    if(command == "PING" || command == "ping")
-        connection.write("+PONG\r\n");
-  })
+    connection.id = id;
+	id += 1;
+
+    connection.on("data", (data) => {
+        let command = parseIncomingData(data);
+        if(command[2] == "PING" || command[2] == "ping")
+            connection.write("+PONG\r\n");
+    })
 });
 
 server.listen(6379, "127.0.0.1");
